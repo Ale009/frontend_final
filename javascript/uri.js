c0dc5572a7cfa0efd431c9f2daada6fe02b90356
Alejandra <salazarale9@hotmail.com>
2025-06-13 20:18:37 -0600

    Update uri.js

--- javascript/uri.js
+++ javascript/uri.js
@@ -1,2 +1,2 @@
-//export const URI = 'https://api-flask-render-l6he.onrender.com'
-export const URI = 'http://localhost:5000' // Uncomment this line to use the local Flask server
+export const URI = 'https://backend-final-cc0n.onrender.com'
+//export const URI = 'http://localhost:5000' // Uncomment this line to use the local Flask server
